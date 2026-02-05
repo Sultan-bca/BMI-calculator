d436ecc5dd7a84335a942580668c9d1c47170e74
Sultan <sultan965499sk@gmail.com>
2026-02-05 12:33:00 +0530

    Update Bmi.jsx

--- src/Components/Bmi/Bmi.jsx
+++ src/Components/Bmi/Bmi.jsx
@@ -85,26 +85,4 @@ const Bmi = () => {
 
 export default Bmi;
 
-/*
 
-
-
-
-      <div classNameName="">
-            
-<form>
-  <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Enter your weight</label>
-    <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-    
-  </div>
-  <div className="mb-3">
-    <label for="exampleInputPassword1" className="form-label">Enter your height</label>
-    <input type="number" className="form-control" id="exampleInputPassword1"/>
-  </div>
-
- <button type="submit" className="btn btn-primary">Submit</button>
- <button>Reload</button>
-</form>
-    </div>
-    */
